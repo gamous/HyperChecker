a43bf204c646e7ecd666de81e31f83271a5ff567
gamous <gammarayburst@163.com>
2023-10-13 15:14:01 +0800

    update x32 build

--- build_x32.bat
+++ build_x32.bat
@@ -1,3 +1,4 @@
 @call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat"
 ml /Fo HvChk_asm_32.obj /c HvChk_x32.asm
 cl HvChk.cpp HvChk_asm_32.obj  /FeHvChk_x32.exe -std:c++14 /EHsc
+del *.obj
